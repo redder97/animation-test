eb1ad2dd64252ca2f954f5ad93055b59cbaa2b6b
redder97 <red9gb@gmail.com>
2023-03-27 11:56:05 +0800

    set animation movement relative to direction of slider adjustment

--- main.js
+++ main.js
@@ -1,14 +1,37 @@
 const imgDisplay = document.getElementById('image');
+
+currentIndex = 1;
 let lastPosition = 0;
 
+
+
+imgDisplay.addEventListener('mouseup', function () {
+    console.log('click')
+})
+
 function onslide(e) {
-    console.log('trigger')
-    const slider = document.getElementById('range')
-    const val = e;
+    const slider = document.getElementById('range');
+    // const val = e;
 
-    slider.setAttribute('value', lastPosition)
-    imgDisplay.setAttribute('src', `images/${val}.jpg`);
+    // slider.setAttribute('value', lastPosition)
+    // imgDisplay.setAttribute('src', `images/${val}.jpg`);
 
-    lastPosition = val;
+    // lastPosition = val;
+
+    if (e < lastPosition) {
+        currentIndex -= 1;
+    } else if (e > lastPosition) {
+        currentIndex += 1;
+    }
+
+    lastPosition = e;
+
+    if (currentIndex < 1) currentIndex = 1;
+    if (currentIndex > 179) currentIndex = 179;
+
+
+    imgDisplay.setAttribute('src', `images/${currentIndex}.jpg`)
+
 
 }
+
